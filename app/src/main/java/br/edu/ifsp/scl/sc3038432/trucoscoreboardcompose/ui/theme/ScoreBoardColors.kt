29d61df92d279fc0bbb070269dec46e6954e2c60
hikazudani <lhjundi@proton.me>
2026-08-19 02:53:24 -0300

    feat(colors): define team and ui background colors

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/trucoscoreboardcompose/ui/theme/ScoreBoardColors.kt
@@ -0,0 +1,10 @@
+package br.edu.ifsp.scl.sc3038432.trucoscoreboardcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ColorTitleBg = Color(0xFF212121)
+val ColorTeamA = Color(0xFF0072B2)
+val ColorTeamAButton = Color(0xFF00497A)
+val ColorTeamB = Color(0xFFE69F00)
+val ColorTeamBButton = Color(0xFFA87200)
+val ColorReset = Color(0xFF424242)
